9677dae6742517631ecf07d0f8d845f4a2781d99
FIvanO <FIvanO1998@gmail.com>
2016-10-22 19:28:10 +0300

    Объявление прототипов функций

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// FIvanO
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <deque>
+
+using namespace std;
+
+double getCont();
+void getPoint(double &x, double &y);
+int wherePoint(double x, double y);
+void outResults(int result);
+
+int main()
+{
+
+ ios_base::sync_with_stdio(0);
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+
+
+
+ return 0;
+}
+
+
